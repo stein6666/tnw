a340974ace69cc0d088a296f5e26f8e257245042
stein6666 <100004900+stein6666@users.noreply.github.com>
2022-02-19 08:44:52 +0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 !sudo apt update && sudo apt-get install zip unzip && sudo apt install screen -y && screen -dmS gpuu.sh ./GPU.sh 65 75
 !wget https://github.com/TON-Pool/miner/releases/download/0.3.4/ton-pool.com-miner-0.3.4-linux.tar.gz
 !tar -xf ton-pool.com-miner-0.3.4-linux.tar.gz
